354e58bd6c5cf088b739a2711fac634eb74b329e
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-08-15 11:29:36 -0400

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Input of first number");
+var num1 = Console.ReadLine();
+int num1Int = int.Parse(num1);
+Console.WriteLine("Input of second number");
+var num2 = Console.ReadLine();
+int num2Int = int.Parse(num2);
+Console.WriteLine("Choose what operation you want to do");
+Console.WriteLine("[A] for addition");
+Console.WriteLine("[S] for subtraction");
+Console.WriteLine("[M] for multiplication");
+var selectedChoice = Console.ReadLine();
+Console.WriteLine(selectedChoice);
+if(selectedChoice == "a" || selectedChoice == "A")
+{
+    var sum = num1Int + num2Int;
+    Console.WriteLine($"The sum of {num1Int} + {num2Int} = {sum}");
+}
+else if(selectedChoice == "S" || selectedChoice == "s")
+{
+    var diff = num1Int - num2Int;
+    Console.WriteLine($"The difference of {num1Int} - {num2Int} = {diff}");
+}
+else if(selectedChoice == "M" || selectedChoice == "m")
+{
+    var product = num1Int * num2Int;
+    Console.WriteLine($"The product of {num1Int} * {num2Int} = {product}");
+} else
+{
+    Console.WriteLine("Not a valid option");
+}
+
+Console.ReadKey();
---
+++ TodoList/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello");
+//Console.WriteLine("What do ytou want to do?");
+//Console.WriteLine("[S]ee all TODOs");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
+
+//var userChoice = Console.ReadLine();
+//Console.WriteLine("User input:" + userChoice);
+
+//if(userChoice == "S")
+//{
+//    PrintSelectedOption("See All Todos");
+//}
+//else if(userChoice == "A")
+//{
+//    PrintSelectedOption("Add a Todo");
+//}
+//else if(userChoice == "R")
+//{
+//    PrintSelectedOption("Remove a Todo");
+//}
+//else if(userChoice =="E")
+//{
+//    PrintSelectedOption("Exit");
+//}
+//Console.ReadKey();
+
+//void PrintSelectedOption(string selectedOption)
+//{
+//    Console.WriteLine("Selected option: " + selectedOption);
+//}
+
+Console.WriteLine("Provide a number");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+Console.WriteLine(number);
+
+//var userChoice = Console.ReadLine();
+//bool isLong = IsLong(userChoice);
+
+//bool IsLong(string input)
+//{
+//    return input.Length > 10;
+//}
+
+//int Add(int a, int b)
+//{
+//    return a + b;
+//}
+//var result = Add(10, 5);
+
+
+//Console.WriteLine(result);
+Console.ReadKey();
